ef5d23e65e3ba8f8b675c30ec478592e27a51f19
demigod66 <180402104@student.umri.ac.id>
2021-07-13 03:15:15 +0700

    pembuatan login

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
-    return view('backend.index');
+    return view('auth.login');
 });
 
 Route::get('/backend/home', function () {
     return view('backend.index');
 });
-
 Route::resource('/backend/category', 'CategoryController');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');

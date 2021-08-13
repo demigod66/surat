6fa74e78d0b095cb0235bc8de8e5c893ef1533c0
demigod66 <180402104@student.umri.ac.id>
2021-08-13 10:44:04 +0700

    update

--- resources/views/backend/header.blade.php
+++ resources/views/backend/header.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>AdminLTE 3 | Dashboard</title>
+  <title>SI-MART - SMAN 1 BANGKO</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -37,6 +37,6 @@
 
   <!-- Preloader -->
   <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="{{ asset('backend/assets/dist/img/AdminLTELogo.png')}}" alt="AdminLTELogo" height="60" width="60">
+    <img class="animation__shake" src="{{ asset('backend/assets/dist/img/smansabanko.png')}}" alt="AdminLTELogo" height="60" width="60">
   </div>
 

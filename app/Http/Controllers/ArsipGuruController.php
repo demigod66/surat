026542bbfd53c17ef4af17576e6fe3739ca5f44f
demigod66 <180402104@student.umri.ac.id>
2021-08-16 14:17:07 +0700

    asdasd

--- app/Http/Controllers/ArsipGuruController.php
+++ app/Http/Controllers/ArsipGuruController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\{User, ArsipGuru};
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Str;
 use Yajra\DataTables\Facades\DataTables;
 use Illuminate\Support\Facades\Session;
 
--- resources/views/backend/sidebar.blade.php
+++ resources/views/backend/sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="/halaman" class="brand-link">
+    <a href="/home" class="brand-link">
       <img src="{{ asset('backend/assets/dist/img/smansabanko.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">SI-MART</span>
 
